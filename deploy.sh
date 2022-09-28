adf75a6979cedcb6df3588153c0ab6e45200d687
Marcelo Gutierrez <mmgc84@gmail.com>
2022-09-28 14:33:39 -0600

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mmgc84/multi-client:latest -t mmgc84/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mmgc84/multi-server:latest -t mmgc84/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mmgc84/multi-worker:latest -t mmgc84/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push mmgc84/multi-client:latest
+docker push mmgc84/multi-server:latest
+docker push mmgc84/multi-worker:latest
+
+docker push mmgc84/multi-client:$SHA
+docker push mmgc84/multi-server:$SHA
+docker push mmgc84/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=mmgc84/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mmgc84/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mmgc84/multi-worker:$SHA
